a778d07acbed14ba7137a922bdd934302290a897
elika2004 <elikatavakoli6561@gmail.com>
2024-10-27 10:58:42 +0330

    update

--- root/views.py
+++ root/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
 def home(request):
-    return HttpResponse('<h2>hello<h2>')
+    return render (request,"root/index.html")
 
 def contact(request):
-    return HttpResponse('contact us')
+    return render (request,"root/contact.html")
 
 def about(request):
-    return HttpResponse('about us')
+    return render (request ," root/about.html")
